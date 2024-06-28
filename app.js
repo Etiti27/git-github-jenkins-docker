c74f7d512906129cb1913ead3adbe7e61ffb1d5f
etiti <christopherobinna27@gmail.com>
2024-06-28 23:32:17 +0200

    image rename

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('we are docker, github, git');
+    res.send('we are docker, github, git repo');
 })
 
 app.listen(3001, (err, res) => {console.log('connected to port 3001');})
